e8e89a39fa2096a022d0ecd5e309e161e32b60d2
sotowang <sotowang@qq.com>
2018-09-16 17:10:19 +0800

    initial commit

---
+++ src/main/java/com/hadoop/JoinMapper.java
@@ -0,0 +1,47 @@
+package com.hadoop;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+
+import java.io.IOException;
+
+public class JoinMapper extends Mapper<LongWritable,Text,Text,Text>{
+    public static final String LEFT_FILENAME = "student_info.txt";
+    public static final String RIGHT_FILENAME = "student_class_info.txt";
+    public static final String LEFT_FILENAME_FLAG = "l";
+    public static final String RIGHT_FILENAME_FLAG = "r";
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        //获取记录的HDFS路径
+        String filePath = ((FileSplit) context.getInputSplit()).getPath().toString();
+        System.out.println("------->FilePath =  " + filePath);
+        String fileFlag = null;
+        String joinKey = null;
+        String joinValue = null;
+
+        //判断记录来自哪个文件
+        if (filePath.contains(LEFT_FILENAME)) {
+            fileFlag = LEFT_FILENAME_FLAG;
+            joinKey = toString().split("\t")[1];
+            System.out.println("------->JoinKey =  " + joinKey);
+            joinValue = toString().split("\t")[0];
+            System.out.println("------->JoinValue =  " + joinValue);
+
+        } else if (filePath.contains(RIGHT_FILENAME)) {
+            fileFlag = RIGHT_FILENAME_FLAG;
+            joinKey = value.toString().split("\t")[0];
+            System.out.println("------->JoinKey =  " + joinKey);
+            joinValue = toString().split("\t")[1];
+            System.out.println("------->JoinValue =  " + joinValue);
+
+
+        }
+        //输出键值对并标示该结果来自哪个文件
+        context.write(new Text(joinKey), new Text(joinValue + "\t" + fileFlag));
+
+    }
+}
+
---
+++ src/main/java/com/hadoop/JoinReducer.java
@@ -0,0 +1,41 @@
+package com.hadoop;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class JoinReducer extends Reducer<Text, Text, Text, Text> {
+    public static final String LEFT_FILENAME = "student_info.txt";
+    public static final String RIGHT_FILENAME = "student_class_info.txt";
+    public static final String LEFT_FILENAME_FLAG = "l";
+    public static final String RIGHT_FILENAME_FLAG = "r";
+
+    @Override
+    protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+
+        Iterator<Text> iterator = values.iterator();
+        List<String> studentClassNames = new ArrayList<String>();
+        String studentName = "";
+
+        while (iterator.hasNext()) {
+            String[] infos = iterator.next().toString().split("\t");
+            //判断该条记录来自哪个文件，并根据文件格式解析记录获取相应的信息
+            if (infos[1].equals(LEFT_FILENAME_FLAG)) {
+                studentName = infos[0];
+            } else if (infos[1].equals(RIGHT_FILENAME_FLAG)) {
+                studentClassNames.add(infos[0]);
+            }
+        }
+
+        //求笛卡儿积
+        for (int i = 0; i < studentClassNames.size(); i++) {
+            context.write(new Text(studentName), new Text(studentClassNames.get(i)));
+
+        }
+
+    }
+}
